e9974b0aa8802c4d31c5f207c23e3c7bd6d03f78
Mango <mango@localhost.localdomain>
2017-05-04 00:34:23 +0200

    example

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-import bar from './bar';
-
+import bar from "./bar";
+import React from "react";// eslint-disable-line
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById("root")
+);
 bar();
--- bar.js
+++ bar.js
@@ -1,6 +1,6 @@
 export default function bar() {
-  var btn = document.createElement("Button");
-  var t = document.createTextNode("Click me!");
-  btn.appendChild(t);
-  document.body.appendChild(btn);
+	var btn = document.createElement("Button");
+	var t = document.createTextNode("Click me!");
+	btn.appendChild(t);
+	document.body.appendChild(btn);
 }

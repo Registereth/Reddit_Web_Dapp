e120ef939b0188bb9149131fa639313742163a12
Mango <mango@localhost.localdomain>
2017-05-04 00:34:19 +0200

    Setup packages and linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-  entry: './app.js',
-  output: {
-    filename: 'bundle.js'
-  }
-}
+module.exports = {// eslint-disable-line
+	entry: "./app.js",
+	output: {
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: ["env","es2015", "react"]
+				}
+			}
+		}]
+	}
+};

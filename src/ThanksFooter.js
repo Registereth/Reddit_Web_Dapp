a1731808061f1ac1c4ba74f78a912c6a3adb53d9
Mango <mango@localhost.localdomain>
2017-05-09 22:02:50 +0200

    Stylistic changes as per request

--- src/ThanksFooter.js
+++ src/ThanksFooter.js
@@ -6,8 +6,8 @@ import React, { Component } from "react";
 const ThanksFooter = () => {
 	return (
 		<footer className="md-paper md-paper--2 md-toolbar--text-white md-text-center fixedFooter" style={{zIndex:"-1"}}>
-			<p> Thanks to the Sponsors: u/maxxFlyer, u/madvas, Joe Urgo, Jarred Case </p>
-			<p> Thanks to the devs: Adam Dossa, Roland Kofler, Kingcocomango </p>
+			<p style={{color:"white"}}> Thanks to the Sponsors: u/maxxFlyer, u/madvas, Joe Urgo, Jarred Case </p>
+			<p style={{color:"white"}}> Thanks to the devs: Adam Dossa, Roland Kofler, Kingcocomango </p>
 		</footer>
 	);
 };

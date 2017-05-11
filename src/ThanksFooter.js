fbbfc02b7386bb84c81b22de4aecf2fe0f0375d0
Mango <mango@localhost.localdomain>
2017-05-11 20:07:53 +0200

    Modified/Updated footer

--- src/ThanksFooter.js
+++ src/ThanksFooter.js
@@ -6,8 +6,8 @@ import React, { Component } from "react";
 const ThanksFooter = () => {
 	return (
 		<footer className="md-paper md-paper--2 md-toolbar--text-white md-text-center fixedFooter" style={{zIndex:"-1"}}>
-			<p style={{color:"white"}}> Thanks to the Sponsors: u/maxxFlyer, u/madvas, Joe Urgo, Jarred Case </p>
-			<p style={{color:"white"}}> Thanks to the devs: Adam Dossa, Roland Kofler, Kingcocomango </p>
+			<p style={{color:"white"}}> Thanks to the sponsors: u/maxxFlyer, Matus Lestan, Joe Urgo, Jarred Case, Shaun Shutte, Roland Kofler </p>
+			<p style={{color:"white"}}> Thanks to the contributors: Adam Dossa, Roland Kofler, u/kingcocomango, Shaun Shutte, u/maxxflyer, Richardo Guilherme Schmidt </p>
 		</footer>
 	);
 };
